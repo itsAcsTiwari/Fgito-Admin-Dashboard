e45c0809cdc12e9395f9759b700485f3192de9a5
devansh <devanshwalia9898@gmail.com>
2023-03-16 01:56:39 +0530

    config tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="overflow-x-hidden overflow-y-auto">
-      <h1 className="font-bold text-2xl">hello world</h1>
+      <h1 className="font-bold text-2xl text-primary">hello world</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3BA738",
+          100: "#b1dcaf",
+          200: "#89ca88",
+          300: "#62b960",
+          400: "#3BA738",
+          500: "#2F862D",
+          600: "#236422",
+          700: "#184316",
+        },
+        grey: {
+          DEFAULT: "#D9D9D9",
+          light: "e8e8e8",
+          dark: "828282",
+        },
+        tint: {
+          DEFAULT: "#5089B2AB",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
 };
