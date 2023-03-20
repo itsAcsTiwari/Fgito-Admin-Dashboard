8ca8a4495bf40c1895e58c8a0dcc22ee6caf3abd
devansh <devanshwalia9898@gmail.com>
2023-03-20 18:47:17 +0530

    update for react query

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,32 +1,37 @@
+'use client'
 import './globals.css'
 
-import { Footer,Header, SideBarNav } from '@src/components'
-import { Inter,Roboto,Roboto_Condensed } from 'next/font/google'
+import { Footer, SideBarNav } from '@src/components'
+import { Roboto, Roboto_Condensed } from 'next/font/google'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 export const metadata = {
 	title: 'Admin web',
 	description: 'To manage fgito data',
 }
 
-const roboto = Roboto({ 
+const roboto = Roboto({
 	subsets: ['latin'],
-	weight: ['400'] 
+	weight: ['400'],
 })
-const robotoCondensed = Roboto_Condensed({ 
+const robotoCondensed = Roboto_Condensed({
 	subsets: ['latin'],
-	weight: ['400'] 
+	weight: ['400'],
 })
+const queryClient = new QueryClient({})
 
 const RootLayout = ({ children }) => {
 	return (
 		<html lang="en" className={roboto.className}>
 			<body className="xl:max-w-screen-2xl mx-auto">
 				{/* <Header /> */}
-				<div className="h-screen flex flex-row justify-start" >
-					<SideBarNav/>
-					<div className="bg-gray-200 flex-1 p-4 text-green-300">
-						{children}
-					</div>
+				<div className="h-screen flex flex-row justify-start">
+					<SideBarNav />
+					<QueryClientProvider client={queryClient}>
+						<div className="bg-gray-200 flex-1 p-4 text-green-300">{children}</div>
+						<ReactQueryDevtools initialIsOpen={false} />
+					</QueryClientProvider>
 				</div>
 				<Footer />
 			</body>
