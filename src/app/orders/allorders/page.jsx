3ff7fa2b0f4e14a99da6f6db2e8998e654a88d58
Tanmay Jauhari <85300765+Tanmay123-ttt@users.noreply.github.com>
2023-03-25 14:05:22 +0530

    updated orders

--- src/app/orders/allorders/page.jsx
+++ src/app/orders/allorders/page.jsx
@@ -1,40 +1,8 @@
-// "use client"
-// import React from 'react'
-// import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-// import {Loader} from "@src/components"
-
-// const Page = () => {
-//     const { isLoading, error, data } = useQuery('repoData', () =>
-// 	fetch('/api/orders/allorders').then(res =>
-// 	  res.json()
-// 	)
-//   )
-//   	if (isLoading) return<Loader />
-// 	if (error) return <ErrorComponent error={error} />
-//   return (
-//     <>
-// 			<div><h1>OrderId</h1></div>
-// 			{
-// 				data?.data.map((data1,idx) => {
-// 					return (
-// 						<div key={idx}>
-// 							<h3>{data1.orderNo}</h3>
-// 						</div>
-// 					)
-
-// 				})
-// 			}
-
-// 		</>
-//   )
-// }
-
-// export default Page;
-
 'use client'
 import { ErrorComponent, Loader } from '@src/components'
-import OrdersTable from '@src/components/orders/ordersTable'
 import { useQuery } from 'react-query'
+
+import OrdersTable from '@src/components/orders/ordersTable'
 
 const Page = () => {
 	const { isLoading, error, data } = useQuery('repoData', () =>
@@ -46,7 +14,7 @@ const Page = () => {
 
 	return (
 		<div>
-			<h1>Order</h1>
+			<h1>Orders</h1>
 			<OrdersTable data={data} />
 		</div>
 	)
