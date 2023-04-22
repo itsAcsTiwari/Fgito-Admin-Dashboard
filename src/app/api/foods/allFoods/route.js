7b9b92aca077acb334a4916daec6b830d4a03a7d
devansh <devanshwalia9898@gmail.com>
2023-04-23 00:49:28 +0530

    fix/next router

--- src/app/api/foods/allFoods/route.js
+++ src/app/api/foods/allFoods/route.js
@@ -1,18 +1,12 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getFood`, {
-        headers: {
-            'Content-Type': 'application/json',
-            authToken: `${process.env.NEXT_PUBLIC_API_KEY}`,
-        },
-    })
-    const data = await res.json()
-    return NextResponse.json({ data })
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getFood`, {
+		headers: {
+			'Content-Type': 'application/json',
+			authToken: `${process.env.NEXT_PUBLIC_API_KEY}`,
+		},
+	})
+	const data = await res.json()
+	return NextResponse.json({ data })
 }
-
-
-
-
-
-
--- src/components/homechef/listAllfood.jsx
+++ src/components/homechef/listAllfood.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { ErrorComponent, Loader } from '@src/components'
 import { Select, TimePicker } from 'antd'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { useQuery } from 'react-query'
 
