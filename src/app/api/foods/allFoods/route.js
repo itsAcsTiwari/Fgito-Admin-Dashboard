aeb08fc6397b7944ed55000e2cda6b97a2d8a3c4
Tanmay <www.tanmayjauhari@gmail.com>
2023-04-24 13:54:42 +0530

    Updated route files

--- src/app/api/foods/allFoods/route.js
+++ src/app/api/foods/allFoods/route.js
@@ -1,14 +1,66 @@
+// import { NextResponse } from 'next/server'
+
+// export async function GET() {
+// 	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getFood`, {
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 			authToken: `${process.env.NEXT_PUBLIC_API_KEY}`,
+// 		},
+// 	})
+// 	const data = await res.json()
+// 	return NextResponse.json({ data })
+// }
+
+
+// import { NextResponse } from 'next/server'
+
+// export async function POST(request) {
+// 	const myHeaders = new Headers()
+// 	myHeaders.append('Content-Type', 'application/json')
+// 	myHeaders.append('authToken', `${process.env.NEXT_PUBLIC_API_KEY}`)
+
+// 	const body = await request.json()
+
+// 	const requestOptions = {
+// 		method: 'POST',
+// 		headers: myHeaders,
+// 		body: JSON.stringify(body),
+// 		redirect: 'follow',
+// 	}
+
+// 	try {
+// 		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getFood`, requestOptions)
+// 		const result = await response.json()
+// 		return NextResponse.json({ data: result })
+// 	} catch (error) {
+// 		console.dir('error', error)
+// 		return NextResponse.error()
+// 	}
+// }
+
 import { NextResponse } from 'next/server'
 
-export async function GET() {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getFood`, {
-		headers: {
-			'Content-Type': 'application/json',
-			authToken: `${process.env.NEXT_PUBLIC_API_KEY}`,
-		},
-	})
-	const data = await res.json()
-	return NextResponse.json({ data })
+export async function POST(request) {
+	const myHeaders = new Headers()
+	myHeaders.append('Content-Type', 'application/json')
+
+	const body = await request.json()
+
+	const requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: JSON.stringify(body),
+		redirect: 'follow',
+	}
+
+	try {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/homeChef/getFood?authToken=${process.env.NEXT_PUBLIC_API_KEY}`, requestOptions)
+		const result = await response.json()
+		return NextResponse.json({ data: result })
+	} catch (error) {
+		console.dir('error', error)
+		return NextResponse.error()
+	}
 }
 
 
