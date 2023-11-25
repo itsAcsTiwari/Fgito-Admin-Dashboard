d5fb5d484e33fa9980736ba4d47250dfdeadb72f
Dharmvir Dharmacharya <ddharmacharya22@gmail.com>
2023-11-26 04:21:07 +0530

    new routes fixed

--- src/core/constants.js
+++ src/core/constants.js
@@ -2,18 +2,18 @@ export const ORDERS = '/orders'
 export const HOMECHEFS = '/homechefs'
 
 export const PageRoutes = {
-	HOME: '/',
-	All_HOME_CHEFS: '/homeChefs/allHomeChefs',
-	ADD_HOME_CHEFS: '/homeChefs/addHomeChefs',
-	ALL_ORDERS: '/orders/allOrders',
-	ADD_ORDERS: '/orders/addOrders',
-	SALES: '/sales',
+  HOME: '/',
+  All_HOME_CHEFS: '/homechefs/allHomeChefs',
+  ADD_HOME_CHEFS: '/homechefs/addHomeChefs',
+  ALL_ORDERS: '/orders/allOrders',
+  ADD_ORDERS: '/orders/addOrders',
+  SALES: '/sales',
 }
 
 export const ApiRoutes = {
-	allHomechefs: '/api/homechefs/allhomechefs',
-	addHomechef: '/api/homechefs/addHomeChefs',
-	allOrders: '/api/orders/allorders',
-	addOrder: '/api/orders/addorder',
-	updateOrder: '/api/orders/updateOrder',
+  allHomechefs: '/api/homechefs/allhomechefs',
+  addHomechef: '/api/homechefs/addHomeChefs',
+  allOrders: '/api/orders/allorders',
+  addOrder: '/api/orders/addorder',
+  updateOrder: '/api/orders/updateOrder',
 }
